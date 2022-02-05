b2f6e7fc20bf223177eef91d0a75ea0763d8d098
matt0472 <mattiapedone93@gmail.com>
2022-02-05 17:46:47 +0100

    Start fixing footer

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,3 +1,13 @@
 <footer class="page-footer">
-    
+    <div class="footer-wrapper">
+        <div class="footer-social footer-box">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nemo rerum necessitatibus reprehenderit nisi ea ullam voluptate tempore rem unde, saepe earum culpa molestiae ipsa quos, aspernatur adipisci dolore ipsam?Magni, incidunt officia. Reprehenderit est voluptatum, ab labore non earum odio eligendi! Dolorum, cumque consectetur totam aliquam recusandae explicabo, quae quidem repellendus temporibus, aspernatur sunt at odit. Nulla, delectus libero!
+        </div>
+        <div class="footer-info footer-box">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nemo rerum necessitatibus reprehenderit nisi ea ullam voluptate tempore rem unde, saepe earum culpa molestiae ipsa quos, aspernatur adipisci dolore ipsam?Magni, incidunt officia. Reprehenderit est voluptatum, ab labore non earum odio eligendi! Dolorum, cumque consectetur totam aliquam recusandae explicabo, quae quidem repellendus temporibus, aspernatur sunt at odit. Nulla, delectus libero!
+        </div>
+        <div class="footer-contacts footer-box">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nemo rerum necessitatibus reprehenderit nisi ea ullam voluptate tempore rem unde, saepe earum culpa molestiae ipsa quos, aspernatur adipisci dolore ipsam?Magni, incidunt officia. Reprehenderit est voluptatum, ab labore non earum odio eligendi! Dolorum, cumque consectetur totam aliquam recusandae explicabo, quae quidem repellendus temporibus, aspernatur sunt at odit. Nulla, delectus libero!
+        </div>
+    </div>
 </footer>
