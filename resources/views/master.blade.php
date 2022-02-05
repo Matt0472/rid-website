b5542b1da6556a6be57c99727f8b78d19e398ae3
matt0472 <mattiapedone93@gmail.com>
2022-02-05 16:32:00 +0100

    Add some meta tags

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Mosaic Maker">
+    <meta name="keywords" content="piastrelle, mosaici, artigiano">
+    <meta name="author" content="Mattia Pedone">
     <title>Rid Website</title>
 
     <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
